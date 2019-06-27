0dd512c8f5eb4266e85ffe2473a670aeea7b499c
YannisSach <yannissah@yahoo.gr>
2019-06-27 10:54:52 +0200

    Added results

--- src/exp_autoencoder_depth.py
+++ src/exp_autoencoder_depth.py
@@ -93,13 +93,15 @@ for i, (user, movie) in enumerate(zip(users_train, movies_train)):
     data_mask[user][movie] = 1
 
 
+depths = [[i] for i in range(2,21)]
+depths += [[i,i] for i in range(2,21)]
+depths += [[i,i,i] for i in range(2,21)]
 
-for layers in [[128], [128,128], [128, 128, 128], [64], [64,64], [64, 64, 64],
-        [32], [32,32], [32,32,32], [16], [16,16], [16,16,16], [8], [8,8], [8,8,8]]:
+for layers in depths: 
     model3 = Autoencoder(number_of_users, number_of_movies, layers=layers)
     model3.train(data_zeros, data_mask, users_validation=users_test, movies_validation=movies_test,
                  ratings_validations=ratings_test, n_epochs=200, verbose=False)
 
     preds = model3.predict(data_zeros, users_test, movies_test)
-    score = root_mean_square_error(ratings_test, preds))
+    score = root_mean_square_error(ratings_test, preds)
     print("layers: {} score: {}".format(layers, score))
