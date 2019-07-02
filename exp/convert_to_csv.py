3205f8546024c9c51301dd9bd5b1a84d575f7674
yannis <yannissah@yahoo.gr>
2019-07-02 18:59:49 +0300

    added script for graphs

--- exp/convert_to_csv.py
+++ exp/convert_to_csv.py
@@ -22,12 +22,12 @@ def convert_to_csv(file, regex, columns, output, dry=False):
             df.to_csv(output, index=False)
 
 if __name__ == "__main__": 
-    '''
-    file = 'layers.out'
-    layers_regex = 'layer: (\d+)\n(\d+\.\d+)'
+    
+    file = 'better_autoencoder.out'
+    layers_regex = 'layer: (\d+) score: (\d+\.\d+)'
     column_names = ['Layer_size', 'score']
     convert_to_csv(file, layers_regex, column_names, 'autoencoder_exp_layer.csv', dry=False)
-    '''
+    
 
     '''
     file = 'autoencoder_exp_depth.out'
@@ -35,7 +35,10 @@ if __name__ == "__main__":
     column_names = ['Architecture', 'Score']
     convert_to_csv(file, layers_regex, column_names, 'autoencoder_exp_depth.csv', dry=False)
     '''
+    
+    '''
     file = 'autoencoder_exp_masking_15_15_15.out'
     layers_regex = 'masking: (\d+\.\d+) score: (\d+\.\d+)'
     column_names = ['masking', 'Score']
     convert_to_csv(file, layers_regex, column_names, 'autoencoder_exp_masking.csv', dry=False)
+    '''
---
+++ exp/make_graphs_from_csvs.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import sys
+
+import matplotlib
+from matplotlib.ticker import StrMethodFormatter, NullFormatter
+
+
+fontsize=13
+
+def read_file(filename):
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=",")
+        # return [(float(r[0]), float(r[1])) for r in reader]
+        d = []
+        xlabel = ""
+        ylabel = ""
+        for i, r in enumerate(reader):
+            if i == 0:
+                xlabel = r[0]
+                ylabel = r[1]
+            else:
+                try:
+                    x = int(r[0])
+                except ValueError:
+                    x = float(r[0])
+                y = float(r[1])
+                d.append((x, y))
+        return xlabel, ylabel, d
+
+
+def autoencoder_graph():
+    data = pd.read_csv('autoencoder_exp_layer.csv')
+    data = data[(data.Layer_size >= 4) & (data.Layer_size <=16)]
+
+
+    fig1, ax1 = plt.subplots()
+
+
+    ax1.set_xlabel('Hidden Layer size')
+    ax1.set_ylabel('RMSE')
+
+    ax1.plot(data['Layer_size'], data['score'], '-o', markevery=[np.argmin(data['score'].values)])
+    plt.savefig('autoenc_layer.pdf')
+
+def svdplus_graph(csv_file):
+    pdf_file = '.'.join([csv_file.split('.')[0], 'pdf'])
+    xlabel, ylabel, data = read_file(csv_file)
+    minimum = min(data, key=lambda x: x[1])
+
+    xs, ys = zip(*data)
+    print(xs)
+    print(ys)
+
+    plt.xlabel(xlabel, fontsize=fontsize)
+    plt.ylabel(ylabel, fontsize=fontsize)
+
+    x_ticks_labels = [str(x) for x in xs]
+    plt.xticks(xs, x_ticks_labels)
+    # plt.xticklabels(x)
+
+    plt.plot(xs, ys)
+    plt.plot(minimum[0], minimum[1], marker="o", c="b")
+    plt.savefig(pdf_file)
+
+
+if __name__ == "__main__":
+    msg = "Plotting {} graphs..."
+
+    print(msg.format("Autoencoder"))
+    autoencoder_graph()
+
+    print(msg.format("SVDplus"))
+    svdplus_graph('hidden_size.csv')
+    svdplus_graph('matrix_reg.csv')
+    svdplus_graph('vector_reg.csv')
+
+
